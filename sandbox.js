498e03941d7813c4161da27c38744600bf1de5a6
isyankarcoder <gencvolkan@gmail.com>
2020-12-26 16:09:51 +0300

    ..

---
+++ sandbox.js
@@ -0,0 +1,23 @@
+var character = 'degol';
+var age = 30;
+var isMissiable = false;
+character = 'volkan';
+age = 50;
+isMissiable = true;
+var circ = function (diamter) {
+    return diamter * Math.PI;
+};
+console.log(circ(15));
+//arrays
+var names = ['volkan', 'tolkan', 'genç'];
+names.push('iaynkar');
+console.log(names);
+//names.push(3);
+var numbers = [10, 20, 30, 42];
+numbers.push(55);
+console.log(numbers);
+var mixed = ['Volkanb0', 45, 'genç', 90, 290];
+mixed.push(5);
+mixed.push('aaa');
+mixed[1] = 'volkantolkan';
+console.log(mixed);
---
+++ sandbox.ts
@@ -0,0 +1,45 @@
+let character = 'degol';
+let age = 30;
+let isMissiable = false;
+
+let character2: string;
+let age2: number;
+let isLoggedIn: false;
+let nums: string[];
+
+character = 'volkan';
+age = 50;
+
+isMissiable = true;
+
+const circ = (diamter:number) :number => {
+    return diamter * Math.PI;
+}
+
+console.log(circ(15))
+
+//arrays
+let names = ['volkan', 'tolkan', 'genç'];
+names.push('iaynkar');
+console.log(names);
+//names.push(3);
+let numbers = [10, 20, 30, 42];
+numbers.push(55);
+console.log(numbers);
+
+let mixed = ['IsyankarCoder', 45, 'genç', 90, 290];
+mixed.push(5);
+mixed.push('aaa');
+mixed[1] = 'volkantolkan';
+console.log(mixed);
+
+//objects
+let person = {
+    name: 'Volkan',
+    age: 55,
+    belt: 'black'
+}
+person.name = 'tgenç';
+person.age = 35;
+
+
