7ba50147bee15723ae38c383d7e7df60869ecf5c
isyankarcoder <gencvolkan@gmail.com>
2021-01-11 22:28:28 +0300

    classinheritance

---
+++ classinheritance.js
@@ -0,0 +1,53 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Shape = /** @class */ (function () {
+    function Shape(a) {
+        this.Area = a;
+    }
+    return Shape;
+}());
+var Circle = /** @class */ (function (_super) {
+    __extends(Circle, _super);
+    function Circle() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Circle.prototype.disp = function () {
+        console.log("Bunun areası bu = ", this.Area);
+    };
+    return Circle;
+}(Shape));
+var obj = new Circle(55);
+obj.disp();
+var Root = /** @class */ (function () {
+    function Root() {
+    }
+    return Root;
+}());
+var Child = /** @class */ (function (_super) {
+    __extends(Child, _super);
+    function Child() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Child;
+}(Root));
+var Leaf = /** @class */ (function (_super) {
+    __extends(Leaf, _super);
+    function Leaf() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Leaf;
+}(Child));
+var ch = new Leaf();
+ch.str = "Samsun55";
+console.log(ch.str);
---
+++ classinheritance.ts
@@ -0,0 +1,28 @@
+class Shape{
+    Area: number;
+    constructor(a:number) {
+        this.Area = a;
+    }
+
+}
+
+class Circle extends Shape{
+    disp(): void {
+        console.log("Bunun areası bu = ", this.Area);
+    }
+}
+
+let obj = new Circle(55);
+obj.disp();
+
+
+class Root{
+    str: string;
+}
+
+class Child extends Root{ }
+class Leaf extends Child{ }
+
+let ch = new Leaf();
+ch.str = "Samsun55";
+console.log(ch.str);
